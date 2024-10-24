95c4757ce4d2ac78753563c50f75606c99e109a5
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-24 20:22:36 +0200

    Added character counting for words

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,47 @@
+use std::collections::HashMap;
+
+struct Word {
+    characters: HashMap<char, i32>,
+}
+
+impl Word {
+    fn from(text: String) -> Word {
+        Word {
+            characters: map_characters(&text),
+        }
+    }
+}
+
+fn map_characters(text: &str) -> HashMap<char, i32> {
+    let mut characters = HashMap::new();
+
+    for c in text.to_lowercase().chars() {
+        let count = match characters.get(&c) {
+            Some(count) => count + 1,
+            None => 1,
+        };
+
+        characters.insert(c, count);
+    }
+
+    characters
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn foo() {
+    fn should_count_characters_in_word() {
+        // given
+        let text = "AaaBbc".to_string();
+
+        // when
+        let word = Word::from(text);
+
+        // then
+        assert_eq!(word.characters.get(&'a').unwrap(), &3);
+        assert_eq!(word.characters.get(&'b').unwrap(), &2);
+        assert_eq!(word.characters.get(&'c').unwrap(), &1);
     }
 }
