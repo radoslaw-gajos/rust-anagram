a8a134b525bcded63bce4d31f1337955b3ee5e1a
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-07 23:01:20 +0100

    Moved word_map module to it's own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 type Result<T> = std::result::Result<T, std::io::Error>;
 
 use crate::word::Word;
-use std::collections::HashMap;
 
 mod word;
 mod anagram;
+mod word_map;
 
 pub fn get_anagrams(target_anagram: &str, word_list: Vec<String>) 
     -> Result<Vec<(String, String)>> {
@@ -14,49 +14,7 @@ pub fn get_anagrams(target_anagram: &str, word_list: Vec<String>)
             .map(Word::from)
             .filter(|w| anagram::is_compatible(w, &target_anagram))
             .collect();
-        let _word_map = map_by_length(words);
+        let _word_map = word_map::map_by_length(words);
 
         Ok(vec![("to".to_string(), "do".to_string())])
-}
-
-fn map_by_length(words: Vec<Word>) -> HashMap<usize, Vec<Word>> {
-    let mut map = HashMap::new();
-
-    for word in words {
-        let length = word.value.len();
-        map.entry(length).or_insert(Vec::new());
-        map.get_mut(&length).unwrap().push(word);
-    }
-
-    map
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn should_map_by_length() {
-        // given
-        let words = vec![
-            Word::from("a".to_string()),
-            Word::from("to".to_string()),
-            Word::from("in".to_string()),
-            Word::from("cat".to_string()),
-            Word::from("dog".to_string()),
-            Word::from("god".to_string()),
-        ];
-
-        // when
-        let map = map_by_length(words);
-
-        // then
-        assert_eq!(map.keys().len(), 3);
-        assert!(map.contains_key(&1usize));
-        assert!(map.contains_key(&2usize));
-        assert!(map.contains_key(&3usize));
-        assert_eq!(map.get(&1usize).unwrap().len(), 1usize);
-        assert_eq!(map.get(&2usize).unwrap().len(), 2usize);
-        assert_eq!(map.get(&3usize).unwrap().len(), 3usize);
-    }
 }
