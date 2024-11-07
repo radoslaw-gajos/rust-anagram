a36ac6b2fc095bdc1ce993e0fdc81de21e4a9ed5
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-07 20:54:34 +0100

    Added an alias for Result

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+type Result<T> = std::result::Result<T, std::io::Error>;
+
 pub fn get_anagrams(_target_anagram: &str, _word_list: Vec<String>) 
-    -> Result<Vec<(String, String)>, std::io::Error> {
+    -> Result<Vec<(String, String)>> {
         Ok(vec![("to".to_string(), "do".to_string())])
 }
