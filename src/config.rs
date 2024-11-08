bed94df6b83d620b8316f78b57ab25fd4328e97a
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-08 11:45:08 +0100

    Getting command line arguments

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 type Result<T> = std::result::Result<T, std::io::Error>;
 
 pub struct Config {
@@ -17,7 +19,26 @@ impl Default for Config {
 }
 
 pub fn get_config() -> Result<Config> {
-    let config = Config::default();
+    let mut config = Config::default();
+
+    //let args: Vec<&str> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 4 {
+        panic!("Invalid number of arguments");
+    }
+
+    if args.len() > 1 {
+        if let Some(target_anagram) = &args.get(1) {
+            config.target_anagram = target_anagram.to_string();
+        }
+        if let Some(word_list_file) = &args.get(2) {
+            config.word_list_file = word_list_file.to_string();
+        }
+        if let Some(target_file) = &args.get(3) {
+            config.target_file = target_file.to_string();
+        }
+    }
 
     Ok(config)
 }
